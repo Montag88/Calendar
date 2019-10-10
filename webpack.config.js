118e8adee93f2442115a1560cbf6f056da5b4c0b
Patrick Wong <pwong288@gmail.com>
2019-10-10 12:13:12 -0700

    initial setup of repo with webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'client', 'index.jsx'),
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  resolve: {
+  	extensions: ['.js', '.jsx', '.json', '.css', '.scss'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js, jsx)$/,  // m?js
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+  mode: 'development', // change to production for optimized package
+};
